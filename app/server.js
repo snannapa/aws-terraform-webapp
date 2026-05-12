239a62c96299439950cdc2e8d532ef35ce0203f6
sudheer <nramsudheer@gmail.com>
2026-05-12 21:33:22 +0930

    Initial Node.js web app

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from AWS EC2 deployed using Terraform!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
