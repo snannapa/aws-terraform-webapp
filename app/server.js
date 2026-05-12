c1912d967f7540848b91f1eb5a1d777846c5880c
sudheer <nramsudheer@gmail.com>
2026-05-12 22:21:50 +0930

    sudheer

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS EC2 deployed using Terraform!");
+  res.send("Hello from AWS EC2 deployed using Terraform sudheer!");
 });
 
 app.listen(3000, () => {
